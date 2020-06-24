3b62487872d65d32bafbbc3544481e50df634511
Deon254 <54399677+Deon254@users.noreply.github.com>
2020-06-24 15:07:01 +0300

    Updated admin_agents pages

--- resources/views/pages/admin/admin_agents_create.blade.php
+++ resources/views/pages/admin/admin_agents_create.blade.php
@@ -10,67 +10,58 @@
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"> 
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
-<form>
-  <div class="form-group">
-    <label for="text">Username</label> 
-    <div class="input-group">
-      <div class="input-group-prepend">
-        <div class="input-group-text">
-          <i class="fa fa-address-card"></i>
+    <div class="modal-content">
+      <div class="modal-header">
+           <div class="pull-right">
+            <a class="btn btn-primary" href="/admin/agents"> Back</a>
         </div>
-      </div> 
-      <input id="text" name="text" placeholder="Enter username" type="text" aria-describedby="textHelpBlock" class="form-control">
-    </div> 
-    <span id="textHelpBlock" class="form-text text-muted">Ensure username is unique and only contains alphanumerical characters</span>
-  </div>
-  <div class="form-group">
-    <label for="text1">Email</label> 
-    <div class="input-group">
-      <div class="input-group-prepend">
-        <div class="input-group-text">
-          <i class="fa fa-envelope"></i>
-        </div>
-      </div> 
-      <input id="text1" name="text1" placeholder="Enter email" type="text" class="form-control">
+        <h4 class="modal-title" >Add Agent:</h4>
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+      </div>
+      <div class="modal-body">
+      <div class="row">
+          <div class="col-md-12">
+
+    @if ($errors->any())
+    <div class="alert alert-danger">
+        <strong>Warning!</strong> Please check your input code<br><br>
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
     </div>
-  </div>
-  <div class="form-group">
-    <label for="text3">Phone number</label> 
-    <div class="input-group">
-      <div class="input-group-prepend">
-        <div class="input-group-text">
-          <i class="fa fa-phone"></i>
+@endif
+
+        <form action= "{{ route('agents.store') }}" method="POST" >
+              @csrf
+                  <div class="form-group">
+                      <label for="">Username</label>
+                      <input type="text"  class="form-control" name="merchant_surname" required="true">
+                  </div>
+
+                  <div class="form-group">
+                      <label for="">Email</label>
+                      <input type="email"  class="form-control" name="email" required="true">
+                  </div>
+
+                  <div class="form-group">
+                      <label for="">Phone Number</label>
+                      <input type="number" class="form-control" name="phonenumber" required="true">
+                  </div>
+
+                  <div class="form-group">
+                      <button class="btn btn-success"type="submit" name="submit">Add</button>
+                  </div>
+              </form>
+          </div>
+          </div>
         </div>
-      </div> 
-      <input id="text3" name="text3" placeholder="Enter your phone number" type="text" class="form-control" aria-describedby="text3HelpBlock">
-    </div> 
-    <span id="text3HelpBlock" class="form-text text-muted">E.g +254 789 890 989</span>
-  </div>
-  <div class="form-group">
-    <label for="text2">Password</label> 
-    <div class="input-group">
-      <div class="input-group-prepend">
-        <div class="input-group-text">
-          <i class="fa fa-key"></i>
-        </div>
-      </div> 
-      <input id="text2" name="text2" type="text" class="form-control">
+      
     </div>
+
   </div>
-  <div class="form-group">
-    <label for="select">Type</label> 
-    <div>
-      <select id="select" name="select" class="custom-select" aria-describedby="selectHelpBlock">
-        <option value="rabbit">Owner</option>
-        <option value="duck">Broker</option>
-      </select> 
-      <span id="selectHelpBlock" class="form-text text-muted">Owner or a broker?</span>
-    </div>
-  </div> 
-  <div class="form-group">
-    <button name="submit" type="submit" class="btn btn-primary">Create</button>
-  </div>
-</form>
+</div>
 @endsection
 
 
