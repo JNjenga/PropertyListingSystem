00815b5a25fc04662ff6e49057a9d4c21d2d20bc
david ogalo <ogalodavid10@gmail.com>
2020-07-09 20:01:54 +0300

    some ui changes

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -44,7 +44,7 @@
 @isset($prop1->property_id)
 <section class="mt-10 pt-10">
     <!-- Page Heading -->
-    <div class="text-center">
+    <div class="text-center" id="featured">
     <h3 class="my-4  fontweight-bolder">Featured Listings</h3>
     </div>
 
@@ -54,32 +54,50 @@
         <div class="col-lg-6 mb-4">
             <div class="card h-100">
                 <div class="card-body">
-                    <h4 class="card-title">
-                        <a href="{{ route('listings.show', $prop1->property_id) }}">{{ $prop1->title }} - {{ $prop1->county->county_title }}</a>
-                    </h4>
-                    <p class="card-text"><em>Ksh. {{ $prop1->price }}</em></p>
-
-                    <span class="badge badge-info">Selling</span>
-                </div>
+                @if($prop1->images->count())
+            <img class="img-fluid rounded mb-3 mb-md-0" src="{{ asset('storage/'.$prop1->images[0]->image_path) }}" alt="">
+            @else
+            <img class="img-fluid rounded mb-3 mb-md-0" src="http://placehold.it/700x300" alt="">
+            @endif
+            <h5  class="status text-capitalize">{{ $prop1->type }}&ensp;<span class="badge badge-info">Ksh {{ $prop1->price}}.00</span></h5>
+            <h3 class="text-capitalize">{{ $prop1->title }}</h3>
+            <h5 class="county"><small>{{ $prop1->county->county_title }}</small></h5>
+            <h4 class="location"><i class="fas fa-map-marker-alt"></i>&ensp;<small>{{ $prop1->location }}</small></h4>
+            <p>{!! \Illuminate\Support\Str::limit($prop1->description, $limit = 150, $end = '...') !!}</p>
+            <div class="button">
+            <a class="btn btn-primary" href="{{ route('listings.show_client', $prop1->property_id) }}">View Property</a>        
+            </div>        
+             </div>
             </div>
         </div>
 @isset($prop2->property_id)
         <div class="col-lg-6 mb-4">
-            <div class="card h-100">
+            <d  iv class="card h-100">
                 <div class="card-body">
-                    <h4 class="card-title">
-                        <a href="{{ route('listings.show', $prop2->property_id) }}">{{ $prop2->title }} - {{ $prop2->county->county_title }}</a>
-                    </h4>
-                    <p class="card-text"><em>{{ $prop2->price }}</em></p>
-
-                    <span class="badge badge-info">Rental</span>
-                </div>
+                @if($prop2->images->count())
+            <img class="img-fluid rounded mb-3 mb-md-0" src="{{ asset('storage/'.$prop2->images[0]->image_path) }}" alt="">
+            @else
+            <img class="img-fluid rounded mb-3 mb-md-0" src="http://placehold.it/700x300" alt="">
+            @endif
+            <h5  class="status text-capitalize">{{ $prop2->type }}&ensp;<span class="badge badge-info">Ksh {{ $prop2->price}}.00</span></h5>
+            <h3 class="text-capitalize">{{ $prop2->title }}</h3>
+            <h5 class="county"><small>{{ $prop2->county->county_title }}</small></h5>
+            <h4 class="location"><i class="fas fa-map-marker-alt"></i>&ensp;<small>{{ $prop2->location }}</small></h4>
+            <p>{!! \Illuminate\Support\Str::limit($prop2->description, $limit = 150, $end = '...') !!}</p>
+            <div class="button">
+            <a class="btn btn-primary" href="{{ route('listings.show_client', $prop2->property_id) }}">View Property</a>        
+            </div>        
+             </div>
             </div>
         </div>
+
     </div>
 @endisset
-    <!-- /.row -->
+    <!-- /.row -->      
 </section>
+<div class="button" id="viewmore">
+            <a class="btn btn-primary" href="/listings">View More</a>        
+  </div>  
 @endisset
 <!-- /.row -->
 <section class="mt-10 pt-10">
