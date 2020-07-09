ac08a8e8a2abeab073d6dd9bf3882717eb42bd95
Ben Muchiri <bernard.njoroge@strathmore.edu>
2020-07-09 12:05:58 -0700

    User redirect after login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -26,7 +27,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/admin';
+    //protected $redirectTo = '/admin';
 
     /**
      * Create a new controller instance.
@@ -36,5 +37,21 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function redirectTo()
+    {
+        if (Auth::user()->hasRole('admin')){
+            $this->redirectTo = route('users.index');
+            return $this->redirectTo;
+        }
+        else if (Auth::user()->hasRole('agent')){
+
+        $this->redirectTo = route('listings.index');
+            return $this->redirectTo;
+
+        }
+
+           
     }
 }
